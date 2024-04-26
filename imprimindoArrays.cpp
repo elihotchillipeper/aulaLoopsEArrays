4acdae2d3adf5d2c5d905a220c67d3a7f57c91e5
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-04-26 00:20:29 +0000

    revisão loops em for

---
+++ imprimindoArrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+//Neste exemplo, estamos declarando e inicializando um array de inteiros e usando um loop "for" para percorrer e imprimir cada elemento do array.
+
+int main(){
+
+int array[] = {1, 2, 3, 4, 5, 6, 7};
+
+    for(int i = 0; i < 7; i++){
+        cout << array[i] << endl;
+    }
+
+    return 0;
+}
